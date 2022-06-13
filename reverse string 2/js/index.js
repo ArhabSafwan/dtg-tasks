6eadc2779d8ea8a1fce22d63df0af4fb9e992a76
ArhabSafwan <asafwan72@gmail.com>
2022-06-13 11:37:06 +0600

    clearing the text input after clicking

--- reverse string 2/js/index.js
+++ reverse string 2/js/index.js
@@ -3,5 +3,8 @@
 function reverse(text) {
     for (let i = text.length - 1; i >= 0; --i) {
       document.getElementById("output").innerHTML += text[i];
+     
+      
     }
+    document.getElementById('input').value='';
   }
